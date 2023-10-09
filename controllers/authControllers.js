0ec8c8ef1c9d6caea495a636cff77be4e2a413b1
#Maxime <maxime.marie@gmx.com>
2023-10-09 16:02:02 +0200

    [EDIT][Controllers][Routes]

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,9 @@
+const { BadRequestError } = require('../errors');
+const db = require('../db');
+const { StatusCodes } = require('http-status-codes');
+const { createJWT } = require('../utils/tokenUtils.js');
+const { hashPassword, comparePassword } = require('../utils/passwordUtils.js');
+
 //! gestion de l'enregistrement  utilisateur
 
 //! gestion de la connexion utilisateur
