3abe16134bbbefe0cd4032f07279b754050c1eba
Jason.renault <55652225+jasonrenau@users.noreply.github.com>
2023-10-16 22:34:55 +0200

    [FEAT][FIX] > add eventControllers

--- controllers/rolesControllers.js
+++ controllers/rolesControllers.js
@@ -4,10 +4,8 @@ const { StatusCodes } = require('http-status-codes');
 //! route avec une modération
 
 // getAllRoles
-
-const getAllRoles = async (req, res) => {
-  const { rows: roles } = await db.query('SELECT *FROM roles');
+const getAllRoles = async (_req, res) => {
+  const { rows: roles } = await db.query('SELECT * FROM roles');
   res.status(StatusCodes.OK).json({ roles });
-  console.log(roles);
 };
 module.exports = { getAllRoles };
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,6 +5,7 @@ const router = Router();
 const {
   authenticateUser,
   authorizePermissions,
+  validateUserParams,
 } = require('../middlewares/authenticationMiddleware.js');
 
 const {
@@ -18,32 +19,29 @@ const {
 } = require('../controllers/UsersController');
 
 const {
-  validateUserParams,
   validateUpdateUserInput,
 } = require('../middlewares/validationMiddleware.js');
 
+// afficher tous les utilisateurs
 router.route('/').get(getAllUsers);
 
+// afficher l'utilisateur connecté
 router.use(authenticateUser).route('/currentUser').get(getCurrentUser);
 
+// afficher un utilisateur
+router.route('/:id').get(getSingleUser);
+
+// update activation user
 router
-  .use(authenticateUser)
+  .use(authenticateUser, authorizePermissions('admin', 'moderator'))
   .route('/activation')
-  .get(authorizePermissions('admin', 'moderator'), getAllInactiveUsers);
+  .get(getAllInactiveUsers)
+  .put(updateActivationUser);
 
+// editer un utilisateur
 router
   .use(authenticateUser)
-  .route('/activation/:id')
-  .put(
-    authorizePermissions('admin', 'moderator'),
-    validateUserParams,
-    updateActivationUser
-  );
-
-router
-  .use(authenticateUser)
-  .route('/:id')
-  .get(getSingleUser)
+  .route('/edit')
   .put([validateUserParams, validateUpdateUserInput], updateUser)
   .delete(validateUserParams, deleteUser);
 
