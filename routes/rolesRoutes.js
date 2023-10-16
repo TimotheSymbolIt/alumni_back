fc67f3fd1296d9eb8ab3d936f0e562b566a33092
#Maxime <maxime.marie@gmx.com>
2023-10-16 14:42:07 +0200

    [FIX][MM]

--- routes/rolesRoutes.js
+++
@@ -1,9 +0,0 @@
-// //! routes des roles
-// const { Router } = require('express');
-// const router = Router();
-
-// router.route('/').get(getAllRoles);
-
-// // auth require
-// router.route('/').post(createRole);
-// router.route('/:id').get(getSingleRole).update(updateRole).delete(deleteRole);
